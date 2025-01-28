32caf67dc60b82e7126dc9476dca840c3027c28a
shiv kumar <kumarshiv9031127789@gmail.com>
2025-01-28 20:59:36 +0530

    header added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import style from "./Header.Module.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <div className="logo">
+        <img src="./images/brand_logo.png" alt="logo" />
+      </div>
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
 }
 
 export default Header;
